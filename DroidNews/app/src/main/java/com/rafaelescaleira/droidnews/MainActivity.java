c419265664f8b6e448f5a00c60e22eb177c1cbe3
Rafael Escaleira <rafaelescaleira@Mac-mini.local>
2020-06-06 11:41:58 -0400

    Frameworks e ícone da aplicação

--- DroidNews/app/src/main/java/com/rafaelescaleira/droidnews/MainActivity.java
+++ DroidNews/app/src/main/java/com/rafaelescaleira/droidnews/MainActivity.java
@@ -3,6 +3,8 @@ package com.rafaelescaleira.droidnews;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.ktx.Firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -17,10 +19,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_news, R.id.navigation_user)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
